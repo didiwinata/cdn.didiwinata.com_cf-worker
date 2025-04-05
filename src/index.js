df31e5f78c3c6ea0c636559fff6427076b0e86f7
Didi Winata <didiwt@gmail.com>
2025-04-05 23:22:38 +0700

    add

--- src/index.js
+++ src/index.js
@@ -1,15 +1,66 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
-	},
+  async fetch(request, env, ctx) {
+    const url = new URL(request.url);
+    const width = url.searchParams.get("width");
+    const height = url.searchParams.get("height");
+
+    const resizingOptions = {
+      width: width ? parseInt(width) : undefined,
+      height: height ? parseInt(height) : undefined,
+      quality: 100,
+      format: 'webp'
+    };
+
+    const hiddenImageOrigin = "https://api.dget.top/didiwinata";
+    const imagePath = url.pathname;
+    const imageURL = hiddenImageOrigin + imagePath;
+
+    try {
+      if (url.pathname === "/") {
+        return new Response(`status OK`);
+      }
+      
+      if (url.pathname === "/favicon.ico") {
+        return fetch("https://didiwinata.com/favicon.ico", {
+          headers: { "Cache-Control": "max-age=31536000, immutable" },
+        });
+      }
+
+      const response = await fetch(imageURL, { cf: { image: resizingOptions } });
+
+      if (!response.ok) {
+        throw new Error('Image fetch failed');
+      }
+
+      const newHeaders = new Headers(response.headers);
+
+      newHeaders.set("Cache-Control", "max-age=31536000, immutable");
+      newHeaders.set("Content-Security-Policy", "upgrade-insecure-requests");
+
+      return new Response(response.body, {
+        status: response.status,
+        statusText: response.statusText,
+        headers: newHeaders,
+      });
+    } catch (error) {
+      console.error("Image fetch failed:", error.message);
+      let fallbackURL = `https://ik.imagekit.io/zxn2e1hij${imagePath}`;
+
+      let transformations = [];
+      if (width) {
+        transformations.push(`w-${width}`);
+      }
+      if (height) {
+        transformations.push(`h-${height}`);
+      }
+
+      if (transformations.length > 0) {
+        fallbackURL += `?tr=${transformations.join(",")}`;
+      }
+      
+      console.log(`Redirecting to fallback: ${fallbackURL}`);
+      
+      return Response.redirect(fallbackURL, 302);
+    }
+  },
 };
